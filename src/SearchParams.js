eaeb21c5fe932b9551997664b43205d1f84fdbfe
alifzeroone <alif@zero-one-group.com>
2022-06-19 23:55:29 +0700

    feat: build to production

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -83,14 +83,6 @@ const SearchParams = () => {
         </label>
         <button>Submit</button>
       </form>
-      {/* {pets.map((pet) => (
-        <Pet
-          name={pet.name}
-          animal={pet.animal}
-          breed={pet.breed}
-          key={pet.id}
-        />
-      ))} */}
       <Results pets={pets} />
     </div>
   );
