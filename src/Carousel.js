a710cf8f871ae3c89fb9196b2393bb5cab1066aa
alifzeroone <alif@zero-one-group.com>
2022-06-21 12:10:59 +0700

    feat: carousel handle click image

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import { Component } from "react";
 
 class Carousel extends Component {
@@ -7,7 +9,11 @@ class Carousel extends Component {
   static defaultProps = {
     images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
   };
-
+  handleIndexClick(event) {
+    this.setState({
+      active: +event.target.dataset.index,
+    });
+  }
   render() {
     const { active } = this.state;
     const { images } = this.props;
@@ -19,10 +25,12 @@ class Carousel extends Component {
           {images.length &&
             images.map((photo, index) => (
               <img
+                onClick={this.handleIndexClick}
                 key={photo}
                 src={photo}
                 className={index === active ? "active" : ""}
                 alt="animal thumbnail"
+                data-index={index}
               />
             ))}
         </div>
