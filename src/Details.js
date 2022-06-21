61a0512c2f2754d3d4c89864f440afe1f4a0361a
alifzeroone <alif@zero-one-group.com>
2022-06-21 12:12:25 +0700

    fix: wrong url

--- src/Details.js
+++ src/Details.js
@@ -5,7 +5,7 @@ class Details extends Component {
   state = { loading: true };
   async componentDidMount() {
     const res = await fetch(
-      `http://pets-images.dev-apis.com/pets?id=${this.props.params.id}`
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.params.id}`
     );
     const json = await res.json();
     this.setState(Object.assign({ loading: false }, json.pets[0]));
