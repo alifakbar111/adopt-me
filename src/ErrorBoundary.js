3295ffffbce5566a6c20947097c779e797e66f52
alifzeroone <alif@zero-one-group.com>
2022-06-22 10:38:31 +0700

    feat: error desc

--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -8,7 +8,7 @@ class ErrorBoundary extends Component {
     return { hasError: true };
   }
   componentDidCatch(error, info) {
-    console.error(error, info);
+    console.error("ErrorBoundary caught an error", error, info);
   }
   componentDidUpdate() {
     if (this.state.hasError) {
