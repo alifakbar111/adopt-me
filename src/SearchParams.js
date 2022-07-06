64bf2712e97338b3eae0b291123417f77f91e878
alifakbar111 <alifakbar111@gmail.com>
2022-07-06 17:02:58 +0700

    feat: styling SearchParams

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -28,8 +28,9 @@ const SearchParams = () => {
   }
 
   return (
-    <div className="search-params">
+    <div className="my-0 mx-auto w-11/12">
       <form
+        className="p-10 mb-10 rounded-lg bg-gray-200 shadow-lg flex flex-col justify-center items-center"
         onSubmit={(e) => {
           e.preventDefault();
           requestPets();
@@ -39,6 +40,8 @@ const SearchParams = () => {
           Location
           <input
             id="location"
+            type="text"
+            className="w-60 mb-5 block"
             value={location}
             placeholder="Location"
             onChange={(e) => updateLocation(e.target.value)}
@@ -49,6 +52,7 @@ const SearchParams = () => {
           <select
             id="animal"
             value={animal}
+            className="w-60 mb-5 block"
             onChange={(e) => {
               updateAnimal(e.target.value);
               updateBreed("");
@@ -72,6 +76,7 @@ const SearchParams = () => {
             disabled={!breeds.length}
             id="breed"
             value={breed}
+            className="w-60 mb-5 block"
             onChange={(e) => updateBreed(e.target.value)}
             onBlur={(e) => updateBreed(e.target.value)}
           >
@@ -87,6 +92,7 @@ const SearchParams = () => {
           Theme
           <select
             value={theme}
+            className="w-60 mb-5 block"
             onChange={(e) => setTheme(e.target.value)}
             onBlur={(e) => setTheme(e.target.value)}
           >
@@ -96,7 +102,12 @@ const SearchParams = () => {
             <option value="mediumorchid">Medium Orchid</option>
           </select>
         </label>
-        <button style={{ backgroundColor: theme }}>Submit</button>
+        <button
+          className="rounded px-6 py-2 text-white hover:opacity-50 border-none"
+          style={{ backgroundColor: theme }}
+        >
+          Submit
+        </button>
       </form>
       <Results pets={pets} />
     </div>
