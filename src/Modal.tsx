a2fec33e8b3ce1eb410938782f91db594e430a15
alifakbar111 <alifakbar111@gmail.com>
2022-07-18 16:21:50 +0700

    feat: typescript setup

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -1,16 +1,21 @@
-import React, { useEffect, useRef } from "react";
+import React, { FunctionComponent, MutableRefObject, useEffect, useRef } from "react";
 import { createPortal } from "react-dom";
 
-const Modal = ({ children }) => {
-  const elRef = useRef(null);
+const Modal: FunctionComponent = ({ children }) => {
+  const elRef: MutableRefObject<HTMLDivElement | null>  = useRef(null);
   if (!elRef.current) {
     elRef.current = document.createElement("div");
   }
   useEffect(() => {
     const modalRoot = document.getElementById("modal");
+    if (!modalRoot || !elRef.current) {
+      return;
+    }
     modalRoot.appendChild(elRef.current);
     return () => {
-      modalRoot.removeChild(elRef.current);
+      if (elRef.current) {
+        modalRoot.removeChild(elRef.current);
+      }
     };
   }, []);
 
