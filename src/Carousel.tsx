fe6672725f3fc151fb3e709050d9508c10cabeca
alifakbar111 <alifakbar111@gmail.com>
2022-07-21 16:27:49 +0700

    feat: refactor Carousel

--- src/Carousel.tsx
+++ src/Carousel.tsx
@@ -1,18 +1,27 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import { Component } from "react";
+import { Component, MouseEvent } from "react";
 
-class Carousel extends Component {
+interface IProps {
+  images: string[]
+}
+ 
+class Carousel extends Component<IProps> {
   state = {
     active: 0,
   };
   static defaultProps = {
     images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
   };
-  handleIndexClick = (event) => {
-    this.setState({
-      active: +event.target.dataset.index,
-    });
+  handleIndexClick = (event: MouseEvent<HTMLElement>): void => {
+    if (!(event.target instanceof HTMLElement)) {
+      return;
+    }
+    if (event.target.dataset.index) {
+      this.setState({
+        active: +event.target.dataset.index,
+      });
+    }
   };
   render() {
     const { active } = this.state;
